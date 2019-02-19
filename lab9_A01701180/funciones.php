5528b63b9bf6501eedc83149c088a846573325e3
Marla <a01701180@itesm.mx>
2019-02-18 23:16:05 -0600

    Termine objetivo introphp

---
+++ lab9_A01701180/funciones.php
@@ -0,0 +1,25 @@
+<?php
+
+$array = array(87, 50, 100, 20, 79);
+
+function Promedio($array)
+{
+  $n = strlen($array);
+  $promedio = 0;
+  for($i = 0; $i < strlen($array); $i++) {
+    $promedio += $array[$x];
+  }
+
+  /*$prome = array_sum($arreglo) / count($arreglo);
+    echo $prome ; */
+  $promedio = $promedio/strlen($array)
+  echo $promedio;
+}
+
+
+
+
+
+
+
+?>
---
+++ lab9_A01701180/index.php
@@ -0,0 +1,6 @@
+<?php
+/*Este es el indice donde estan la funciones */
+include("funciones.php");//archivo php de funciones
+include("index.html");//archivo html ligado
+
+?>
